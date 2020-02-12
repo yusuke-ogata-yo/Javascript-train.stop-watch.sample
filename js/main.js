1a80434efbac0b84ebcf96d3c7c72c489806c13d
Yusuke Ogata <yusuke.ogata.yo@gmail.com>
2020-02-12 15:44:15 +0900

    [fix] lesson#08 ボタンを何度も押せないように変更
    stop ボタンや start ボタンを何度も押すと、正しくタイマーをカウントできないため、
    ボタンを押せない(disabled状態)にすることで、ボタンを押せないようにする。

--- js/main.js
+++ js/main.js
@@ -22,17 +22,38 @@
     }, 10);
   }
 
+  function setButtonStateInitial(){
+    start.disabled = false;
+    stop.disabled = true;
+    reset.disabled = true;
+  }
+  function setButtonStateRunning(){
+    start.disabled = true;
+    stop.disabled = false;
+    reset.disabled = true;
+  }
+  function setButtonStateStopped(){
+    start.disabled = false;
+    stop.disabled = true;
+    reset.disabled = false;
+  }
+  
+setButtonStateInitial();
+
   start.addEventListener('click', () => {
+    setButtonStateRunning();
     startTime = Date.now();
     countUp();
   });
 
   stop.addEventListener('click', () => {
+    setButtonStateStopped();
     clearTimeout(timeoutId);
     elapsedTime += Date.now() - startTime;
   });
 
   reset.addEventListener('click', () => {
+    setButtonStateInitial();
     timer.textContent = '00:00.000';
     elapsedTime = 0;
   });
